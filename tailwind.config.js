77f4328832b1e6067b332a9f9303d56c0cc14010
Milind <40736631+milindex@users.noreply.github.com>
2022-03-17 05:11:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     purge: [
         './resources/views/**/*.blade.php',
         './resources/css/**/*.css',
     ],
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
     ],
     theme: {
         extend: {
             colors:{
                 primary: '#E20727',
+                secondary: '#DC3D3A',
                 'primary-gray': '#F8F9F9',
                 'accent-twitter': '#29A2F0',
                 'accent-instagram': '#BF3483',
@@ -19,10 +22,17 @@ module.exports = {
                 'accent-gitlab': '#E04130',
                 'accent-github': '#161B22',
             }
+        },
+        fontFamily: {
+            'sans': ['Raleway', 'ui-sans-serif', 'system-ui', ...defaultTheme.fontFamily.sans],
         }
     },
     variants: {},
     plugins: [
         require('@tailwindcss/ui'),
-    ]
+        require('@tailwindcss/typography'),
+    ],
+    corePlugins: {
+        preflight: true,
+    }
 }
